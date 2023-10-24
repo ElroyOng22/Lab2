81b246983c1cc01c2f99feea4b15c1032f1f35ea
Elroy Ong <ELROYONG.22@ichat.sp.edu.sg>
2023-10-24 10:59:16 +0800

    calculate temperature statistics

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,24 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calc_average_temperature(temperatures):
+    if not temperatures:
+        return 0.0  # Return 0.0 for an empty list
+    return sum(temperatures) / len(temperatures)
+
+
+def calc_min_max_temperature(temperatures):
+    if not temperatures:
+        return [0, 0]  # Return [0, 0] for an empty list
+    return [min(temperatures), max(temperatures)]
+
+
+if __name__ == "__main__":
+    print("Temperature Statistics Calculator")
+
+    # Input temperature data
+    temperature_data = [25.5, 30.2, 28.0]
+
+    average_temp = calc_average_temperature(temperature_data)
+    min_max_temp = calc_min_max_temperature(temperature_data)
+
+    print(f"Average Temperature: {average_temp:.2f}")
+    print(f"Minimum Temperature: {min_max_temp[0]:.2f}")
+    print(f"Maximum Temperature: {min_max_temp[1]:.2f}")
