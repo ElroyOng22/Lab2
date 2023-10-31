124009dc45c3a343995c1f1742554aed83adb3fc
Elroy Ong <ELROYONG.22@ichat.sp.edu.sg>
2023-10-31 10:31:05 +0800

    adding weight classification

--- bmi.py
+++ bmi.py
@@ -1,6 +1,5 @@
 def calculate_bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
+   
 
     # Calculate BMI
     bmi = weight / (height * height)
@@ -9,16 +8,28 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
 
     # Determine weight classification
+    weight_classification = ""
     if bmi < 18.5:
-        print("Weight Classification: Under Weight")
+        weight_classification = "Under Weight"
     elif 18.5 <= bmi <= 25.0:
-        print("Weight Classification: Normal Weight")
+        weight_classification = "Normal Weight"
     else:
-        print("Weight Classification: Over Weight")
+        weight_classification = "Over Weight"
 
-calculate_bmi(weight=57, height=1.73)
+    # Determine weight classification code
+    weight_classification_code = -1
+    if bmi < 18.5:
+        weight_classification_code = 0
+    elif 18.5 <= bmi <= 25.0:
+        weight_classification_code = 1
 
 
 
+    # Return the BMI, weight classification, weight classification code, and health risk
+    return bmi, weight_classification, weight_classification_code
 
+bmi, weight_classification, weight_classification_code,  = calculate_bmi(weight=57, height=1.73)
 
+print("BMI:", bmi)
+print("Weight classification:", weight_classification)
+print("Weight classification code:", weight_classification_code)
